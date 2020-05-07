675e294f8d82abe793496d119a1342d9bc35759f
Hunter Danielson <hunterleedanielson@gmail.com>
2020-05-06 17:13:22 -0700

    undo last commit

--- utils.js
+++ utils.js
@@ -38,7 +38,7 @@ function mungeTrails(trailData) {
                 length: hike.length,
                 conditionStatus: hike.conditionStatus,
                 stars: hike.stars,
-                votes: hike.starVotes,
+                starVotes: hike.starVotes,
                 difficulty: hike.difficulty,
                 picture: hike.imgMedium
             };
